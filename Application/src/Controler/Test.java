f324567219831f5db6f25e8eb2dee1f5056c044c
Gabriel <rougegabriel2@gmail.com>
2023-11-13 09:41:54 +0100

    Commentarisation

---
+++ Application/src/Controler/Test.java
@@ -0,0 +1,17 @@
+package Controler;
+
+import Model.Creature.Caracteristic.Age;
+import Model.Creature.Caracteristic.Sex;
+import Model.Zoo;
+
+/**
+ * The test class of the application.
+ * Entrance point when the developers need to do some tests.
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZooMaster myZooMaster = new ZooMaster("Lucie", Sex.FEMALE, Age.TEEN);
+        Zoo myZoo = Zoo.opening("HelloWorld", myZooMaster);
+        System.out.println(myZoo);
+    }
+}
