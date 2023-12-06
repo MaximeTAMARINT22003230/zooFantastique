aed34a7ca22f825f0e89798608c4ba1c8bf6271d
MaximeTAMARINT22003230 <maxime.tamarin@etu.univ-amu.fr>
2023-12-06 10:08:04 +0100

    ajustement de la classe DiceRoll.java pour qu'un dé à 4 face donne un chiffre de 1 à 4 et non de 0 à 3

--- Application/src/Game/DiceRoll.java
+++ Application/src/Game/DiceRoll.java
@@ -2,21 +2,21 @@ package Model;
 
 public class DiceRoll {
     public static int d4(){
-        return (int)(Math.random() * (5));
+        return (int)(Math.random() * (4))+1;
     }
     public static int d6(){
-        return (int)(Math.random() * (7));
+        return (int)(Math.random() * (6))+1;
     }
     public static int d8(){
-        return (int)(Math.random() * (9));
+        return (int)(Math.random() * (8))+1;
     }
     public static int d12(){
-        return (int)(Math.random() * (13));
+        return (int)(Math.random() * (12))+1;
     }
     public static int d20(){
-        return (int)(Math.random() * (21));
+        return (int)(Math.random() * (20))+1;
     }
     public static int d100(){
-        return (int)(Math.random() * (101));
+        return (int)(Math.random() * (100))+1;
     }
 }
